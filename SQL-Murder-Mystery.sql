dae33795c273f2276032613bbdf3d7f3420b1185
Aga <74723716+agabaj1@users.noreply.github.com>
2023-07-10 22:16:34 +0200

    Update SQL-Murder-Mystery.sql

--- SQL-Murder-Mystery.sql
+++ SQL-Murder-Mystery.sql
@@ -5,21 +5,22 @@ WHERE type = 'murder' AND date = '20180115' AND city = 'SQL City'
 
 --Seeking the witnesses
 --The first witness:
-SELECT * 
+SELECT *
 FROM person 
 WHERE address_street_name = 'Northwestern Dr' 
-ORDER BY address_number desc
+ORDER BY address_number DESC
 LIMIT 1
 
 --The second witness:
 SELECT *
 FROM person
-WHERE address_street_name = "Franklin Ave" AND name like "%Annabel%"
+WHERE address_street_name = 'Franklin Ave' AND name LIKE '%Annabel%'
 
 --Witnesses - interview:
 SELECT p.name, p.id, i.transcript
 FROM interview i
-JOIN person as p ON i.person_id = p.id
+JOIN person as p
+  ON i.person_id = p.id
 WHERE p.id = 14887 OR p.id = 16371
 
 --Checking Morty Schapiro interview:
@@ -27,23 +28,26 @@ SELECT p.name, p.license_id,
 d.id, d.age, d.height, d.eye_color, d.hair_color, d.gender, 
 d.plate_number, d.car_make, d.car_model
 FROM drivers_license d 
-JOIN person p ON p.license_id=d.id
+JOIN person p
+  ON p.license_id=d.id
 WHERE d.plate_number LIKE '%H42W%'
 
 SELECT *
 FROM get_fit_now_member
-WHERE id like '48Z%' AND membership_status='gold'
+WHERE id LIKE '48Z%' AND membership_status = 'gold'
 
 --Checking Annabel Miller interview:
 SELECT m.id, m.person_id, m.membership_status,
 c.check_in_date, c.check_in_time, c.check_out_time
 FROM get_fit_now_check_in c 
-JOIN get_fit_now_member m on m.id = c.membership_id
+JOIN get_fit_now_member m
+  ON m.id = c.membership_id
 WHERE c.check_in_date = 20180109 AND m.membership_status = 'gold' AND m.person_id = 67318
-
+  
 --Check your solution
 INSERT INTO solution VALUES (1, 'Jeremy Bowers')
-SELECT value FROM solution
+SELECT value
+FROM solution
 
 --Murderer - interview:
 SELECT *
@@ -56,31 +60,32 @@ WHERE person_id = 67318
 SELECT *
 FROM person p
 JOIN drivers_license d
-ON p.license_id = d.id
+  ON p.license_id = d.id
 JOIN facebook_event_checkin f
-ON f.person_id = p.id
-WHERE gender = 'female' AND d.car_make = 'Tesla' AND d.car_model = 'Model S' AND f.event_name = 'SQL Symphony Concert'
+  ON f.person_id = p.id
+WHERE gender = 'female' AND d.car_make = 'Tesla' AND d.car_model = 'Model S'
+AND f.event_name = 'SQL Symphony Concert'
 
 --Second option:
 SELECT *
 FROM person
 WHERE id =
-(SELECT person_id
-FROM facebook_event_checkin
-WHERE event_name = 'SQL Symphony Concert' AND date like '201712%'
-GROUP BY person_id
-HAVING COUNT(DISTINCT event_name) = 3)
+  (SELECT person_id
+  FROM facebook_event_checkin
+  WHERE event_name = 'SQL Symphony Concert' AND date like '201712%'
+  GROUP BY person_id
+  HAVING COUNT(DISTINCT event_name) = 3)
 OR
 license_id =
-(SELECT id
-FROM drivers_license
-WHERE gender = 'female' AND hair_color = 'red' AND height between 65 and 67
-AND car_make = 'Tesla' AND car_model = 'Model S')
+  (SELECT id
+  FROM drivers_license
+  WHERE gender = 'female' AND hair_color = 'red' AND height between 65 and 67
+  AND car_make = 'Tesla' AND car_model = 'Model S')
 OR
 id =
-(SELECT ssn
-FROM income
-ORDER BY annual_income)
+  (SELECT ssn
+  FROM income
+  ORDER BY annual_income)
 
 --Check your solution:
 INSERT INTO solution VALUES (1, 'Miranda Priestly')
